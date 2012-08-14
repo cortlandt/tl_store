580a5a8dfe11017a0336a86393705f23184867d7
Cort Johnson <cort@terriblelabs.com>
2012-08-13 20:02:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TlStore::Application.config.secret_token = 'ff2293617d1c6cc8e4fabd5b7866a0e4b8ba27a91cc59711ceec3bd495372f66b35f047a6a2c78d289afaaeb66d5a932b2c1324f7cda611e0c1fba36e0f98cc3'
